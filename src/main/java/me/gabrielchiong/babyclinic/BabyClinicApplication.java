f2299f69fd3330000d2d9a85e086fc3312049420
Gabriel Chiong <gabrielzhenchiong@gmail.com>
2022-05-25 18:14:11 +1000

    init spring project with java files
    Signed-off-by: Gabriel Chiong <gabrielzhenchiong@gmail.com>

---
+++ src/main/java/me/gabrielchiong/babyclinic/BabyClinicApplication.java
@@ -0,0 +1,13 @@
+package me.gabrielchiong.babyclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/gabrielchiong/babyclinic/BabyClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabrielchiong.babyclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
