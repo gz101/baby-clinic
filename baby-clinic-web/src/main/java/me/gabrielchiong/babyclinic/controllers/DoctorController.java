51360abf4cb67ee55cad9935545c1efeb2a9db04
Gabriel Chiong <gabrielzhenchiong@gmail.com>
2022-05-28 19:13:43 +1000

    adding doctors page and controller. Closes #7

---
+++ baby-clinic-web/src/main/java/me/gabrielchiong/babyclinic/controllers/DoctorController.java
@@ -0,0 +1,12 @@
+package me.gabrielchiong.babyclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DoctorController {
+    @RequestMapping({"/doctors", "/doctors/index", "/doctors/index.html"})
+    public String listDoctors() {
+        return "doctors/index";
+    }
+}
