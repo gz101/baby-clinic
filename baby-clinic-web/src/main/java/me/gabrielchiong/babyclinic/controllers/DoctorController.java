6febe66188f482164c0d44c05a694cfdaae1581e
Gabriel Chiong <gabrielzhenchiong@gmail.com>
2022-05-29 16:32:20 +1000

    Added doctor list and updated controller. Closes #15

--- baby-clinic-web/src/main/java/me/gabrielchiong/babyclinic/controllers/DoctorController.java
+++ baby-clinic-web/src/main/java/me/gabrielchiong/babyclinic/controllers/DoctorController.java
@@ -1,12 +1,21 @@
 package me.gabrielchiong.babyclinic.controllers;
 
+import me.gabrielchiong.babyclinic.services.DoctorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DoctorController {
+    private final DoctorService doctorService;
+
+    public DoctorController(DoctorService doctorService) {
+        this.doctorService = doctorService;
+    }
+
     @RequestMapping({"/doctors", "/doctors/index", "/doctors/index.html"})
-    public String listDoctors() {
+    public String listDoctors(Model model) {
+        model.addAttribute("doctors", doctorService.findAll());
         return "doctors/index";
     }
 }
